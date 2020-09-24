d5677a9ece5dfe6bffc637074a64f645d2ea65c1
Prasun Thapliyal <prasun.thapliyal@gmail.com>
2020-09-24 04:14:18 -0700

    Hello World of Antlr

---
+++ CodeGen/CodeGen/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Antlr4.StringTemplate;
+using Antlr4.StringTemplate.Compiler;
+
+namespace CodeGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Template template = new Template("Hello, <name>");
+            template.Add("name", "World");
+
+            var x = template.Render();
+
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ CodeGen/CodeGen/ReadMe.txt
@@ -0,0 +1,10 @@
+﻿24 Sept 2020
+============
+
+Getting Familiar with using Antlr in C#
+
+The Hello, World !
+	1. Visual Studio 2019 -> .NET Core Console Program
+	2. Add Nuget "StringTemplate4"
+	3. Use Antlr.StringTemplate.Template class to write the Hello, World of Antlr
+
