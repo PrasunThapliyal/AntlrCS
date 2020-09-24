d15149a6f6232e79d3a7f8a780340f68a2109be8
Prasun Thapliyal <prasun.thapliyal@gmail.com>
2020-09-24 06:05:23 -0700

    Read Template from a text file - essentially same as Hello World

--- CodeGen/CodeGen/Program.cs
+++ CodeGen/CodeGen/Program.cs
@@ -1,21 +1,64 @@
-﻿using System;
-using Antlr4.StringTemplate;
-using Antlr4.StringTemplate.Compiler;
-
+﻿
 namespace CodeGen
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using Antlr4.StringTemplate;
+    using Antlr4.StringTemplate.Compiler;
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var templateReader = new TemplateReader();
 
-            Template template = new Template("Hello, <name>");
-            template.Add("name", "World");
+            foreach (var template in templateReader.LoadTemplates())
+            {
+                template.Add("name", "World");
+                template.Add("assemblyName", "World");
+                template.Add("namespace", "World");
+                template.Add("className", "World");
+                template.Add("tableName", "World");
+                template.Add("propertyName", "World");
+                template.Add("csType", "World");
+                template.Add("columnName", "World");
+                template.Add("indexName", "World");
+                template.Add("bagName", "World");
+                template.Add("bagFKColumnName", "World");
+                template.Add("bagType", "World");
 
-            var x = template.Render();
+                var x = template.Render();
+                Console.WriteLine(x);
+            }
 
-            Console.WriteLine(x);
         }
+
+    }
+
+    public class TemplateReader
+    {
+        public TemplateReader()
+        {
+
+        }
+
+        public IEnumerable<Template> LoadTemplates()
+        {
+            var assembly = GetType().Assembly;
+            foreach (var resourceName in assembly.GetManifestResourceNames())
+            {
+                using (var stream = assembly.GetManifestResourceStream(resourceName))
+                {
+                    using (var reader = new StreamReader(stream))
+                    {
+                        var text = reader.ReadToEnd();
+                        yield return new Template(text);
+                    }
+                }
+            }
+        }
+
     }
 }
--- CodeGen/CodeGen/ReadMe.txt
+++ CodeGen/CodeGen/ReadMe.txt
@@ -7,4 +7,7 @@ The Hello, World !
 	1. Visual Studio 2019 -> .NET Core Console Program
 	2. Add Nuget "StringTemplate4"
 	3. Use Antlr.StringTemplate.Template class to write the Hello, World of Antlr
+--------------------
+Reading Template from a file
+	Here I'm reading a template file just as a string, so essentially this is similar to the previous example
 
